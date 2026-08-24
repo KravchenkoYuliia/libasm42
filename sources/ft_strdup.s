55bd3cb27c00192ee3c08bc70d070ae12cb9abef
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-08-24 15:29:52 +0200

    feat(ft_strdup): set errno in case of NULL parameter

--- sources/ft_strdup.s
+++ sources/ft_strdup.s
@@ -1,28 +1,39 @@
 
+section	.text
+global	ft_strdup
 extern	ft_strlen
 extern	ft_strcpy
 extern	malloc
-global	ft_strdup
+extern __errno_location
 
-section	.text
+EINVAL equ 22					; 22 errno code = invalid argument
 
 ft_strdup:
 
-	call	ft_strlen		; rax has the length of the given string
+	test	rdi, rdi
+	jz		.string_is_null
 
-	push	rdi				; put char* string to stack to save it for later
+	call	ft_strlen			; rax has the length of the given string
+
+	push	rdi					; put char* string to stack to save it for later
 	
-	mov		rdi, rax		; rdi has length now
+	mov		rdi, rax			; rdi has length now
 	add		rdi, 1
 	call	malloc
 	test	rax, rax
 	jz		.malloc_failed
 
-	mov		rdi, rax		; pointer to the allocated memory
+	mov		rdi, rax			; pointer to the allocated memory
 	pop		rsi
 	call	ft_strcpy
 
 	ret
 
-	.malloc_failed:
+.string_is_null:
+	call	__errno_location
+	mov		[rax], EINVAL
+	mov		rax, 0
+	ret
+
+.malloc_failed:
 		ret
